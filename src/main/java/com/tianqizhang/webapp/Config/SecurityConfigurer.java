5305db33dea4b234955c94beddc57abe622a124f
Ethan Zhang <ethanzhang1997@gmail.com>
2021-10-11 20:11:26 -0400

    clean repo

--- src/main/java/com/tianqizhang/webapp/Config/SecurityConfigurer.java
+++ src/main/java/com/tianqizhang/webapp/Config/SecurityConfigurer.java
@@ -1,10 +1,8 @@
 package com.tianqizhang.webapp.Config;
 
-import com.tianqizhang.webapp.Repo.UserRepo;
 import com.tianqizhang.webapp.Services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
